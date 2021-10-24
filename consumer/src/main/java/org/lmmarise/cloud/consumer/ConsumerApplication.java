893f49a715352efe82d9442f0201a23602cb66ac
lmmarisej <lmmarise@foxmail.com>
2021-10-24 18:57:31 +0800

    feat:consul实现服务治理

--- consumer/src/main/java/org/lmmarise/cloud/consumer/ConsumerApplication.java
+++ consumer/src/main/java/org/lmmarise/cloud/consumer/ConsumerApplication.java
@@ -2,16 +2,15 @@ package org.lmmarise.cloud.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author lmmarise.j@gmail.com
  * @since 2021/10/21 12:34 上午
  */
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableFeignClients
+/*切到consul*/
+//@EnableDiscoveryClient//(autoRegister = false)    // 让服务中心发现、扫描到服务
+//@EnableFeignClients
 public class ConsumerApplication {
 
     public static void main(String[] args) {
--- provider/src/main/java/org/lmmarise/cloud/provider/ProviderApplication.java
+++ provider/src/main/java/org/lmmarise/cloud/provider/ProviderApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @since 2021/10/21 12:57 上午
  */
 @SpringBootApplication
-@EnableDiscoveryClient  // 客户端服务注册和发现功能
+@EnableDiscoveryClient  // 服务发现
 public class ProviderApplication {
 
     public static void main(String[] args) {
