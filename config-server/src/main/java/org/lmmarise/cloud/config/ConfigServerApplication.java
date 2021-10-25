7a1587c6600c22ecdf67b9103eea1248a47c6332
lmmarisej <lmmarise@foxmail.com>
2021-10-26 00:36:37 +0800

    feat:配置中心以及配置文件

---
+++ config-server/src/main/java/org/lmmarise/cloud/config/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package org.lmmarise.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author lmmarise.j@gmail.com
+ * @since 2021/10/26 12:03 上午
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
