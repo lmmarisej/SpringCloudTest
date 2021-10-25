4ed7767c521afc5cb81140f59cf5a896a0337250
lmmarisej <lmmarise@foxmail.com>
2021-10-25 19:07:18 +0800

    feat:Gateway注册到注册中心

--- gateway/src/main/java/org/lmmarise/cloud/gateway/GatewayApplication.java
+++ gateway/src/main/java/org/lmmarise/cloud/gateway/GatewayApplication.java
@@ -2,10 +2,12 @@ package org.lmmarise.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
 
